5d14870382c202260a4e92858e57870c1a5b3929
Ken <10846013@ntub.edu.tw>
2023-08-30 18:40:23 +0800

    Complete HUI miner

--- HUI_Miner/HUI_Miner.py
+++ HUI_Miner/HUI_Miner.py
@@ -1,16 +1,17 @@
-def HUI_Miner(P_UL, UL, minutil):
-    for i in range(len(UL)):
-        print(UL[i])
+"""
+# HUI Miner steps:
+(1) Initial Utility-List
+1. First scan database: Calculate twu and pruned item if the item's twu less than minimum utilities by Property01
+   and then sort item by item's twu-ascending order.
+2. Second scan database: Create revised database by: all the item must belongs to remaining items(First step) in
+   transaction, the remaining items are sorted in twu-ascending order.
+3. Initial Utility-List: contains fields (tid, iutil, rutil).
 
-        exULs = []
-        for j in range(i + 1, len(UL)):
-            exULs.append(P_UL + UL[i][-1] + UL[j][-1])
-        HUI_Miner(UL[i], exULs, minutil)
-
-
-# HUI_Miner('', ['e', 'c', 'b', 'a', 'd'], 20)
-
-
+(2) Construct Algorithm(P, Px, Py) return (Pxy)
+(3) HUI-Miner algorithm(P, ULs, minutil)
+(4) Given a database and minimum utilities, after construct initial Utility-List,
+    HUI-Miner(∅, IULS, minutil) can mine all HUI
+"""
 # Database
 utility_table = {'a': 1, 'b': 2, 'c': 1, 'd': 5, 'e': 4, 'f': 3, 'g': 1}
 transaction_table = [
@@ -66,31 +67,68 @@ for i in transaction_table_transformed:
     for j in i:
         j[1] *= utility_table[j[0]]
 
-print(transaction_table_transformed)
-print(utility_list_transformed)
+# print(transaction_table_transformed)
+# print(utility_list_transformed)
+class UtilityList:
+    def __init__(self, item, UL):
+        self.item = item
+        self.UL = UL
+        pass
+
+IUL_Obj = []
+IUL = []
 for item in utility_list_transformed:
-    print('='*5 + item + '='*5)
+    E = []
     for transaction in range(len(transaction_table_transformed)):
-        E = []
         for items_index in range(len(transaction_table_transformed[transaction])):
             if transaction_table_transformed[transaction][items_index][0] == item:
-                # save item in data structure
-                E = [transaction + 1, transaction_table_transformed[transaction][items_index][1], sum([x[1] for x in transaction_table_transformed[transaction][items_index + 1:]])]
-                print(E)
+                # save item in utility-list
+
+                E.append([transaction + 1, transaction_table_transformed[transaction][items_index][1], sum([x[1] for x in transaction_table_transformed[transaction][items_index + 1:]])])
                 continue
+    IUL_Obj.append(UtilityList(item, E))
+    IUL.append(E)
 
 
-'''
-# HUI Miner steps:
-(1) Initial Utility-List
-1. First scan database: Calculate twu and pruned item if the item's twu less then minimum utilities by Property01
-   and then sort item by item's twu-ascending order.
-2. Second scan database: Create revised database by: all the item must belongs to remaining items(First step) in 
-   transaction, the remaining items are sorted in twu-ascending order.
-3. Initial Utility-List: contains fields (tid, iutil, rutil).
+def Construct(P_UL, Px_UL, Py_UL):
+    Pxy_UL = []
+    Ex_index = 0
+    Ey_index = 0
+    while Ex_index < len(Px_UL) and Ey_index < len(Py_UL):
+        if Px_UL[Ex_index][0] == Py_UL[Ey_index][0]:
+            Exy = []
+            if P_UL is not None:
+                for E in range(len(P_UL)):
+                    if P_UL[E][0] == Px_UL[Ex_index][0]:
+                        Exy = [Px_UL[Ex_index][0], Px_UL[Ex_index][1] + Py_UL[Ey_index][1] - P_UL[E][1], Py_UL[Ey_index][2]]
+            else:
+                Exy = [Px_UL[Ex_index][0], Px_UL[Ex_index][1]+Py_UL[Ey_index][1], Py_UL[Ey_index][2]]
+            Pxy_UL.append(Exy)
+            Ex_index += 1
+            Ey_index += 1
+        else:
+            if Px_UL[Ex_index][0] > Py_UL[Ey_index][0]:
+                Ey_index += 1
+            else:
+                Ex_index += 1
+    return Pxy_UL
 
-(2) Construct Algorithm(P, Px, Py) return (Pxy)
-(3) HUI-Miner algorithm(P, ULs, minutil)
-(4) Given a database and minimum utilities, after construct initial Utility-List, 
-    HUI-Miner(∅, IULS, minutil) can mine all HUI
-'''
+
+def HUI_Miner(P, ULs, minutil):
+    for X in ULs:
+        # print(X.item)
+        if sum([x[1] for x in X.UL]) >= minutil:
+            print(X.item, "Utility:", sum([x[1] for x in X.UL]))
+
+        if sum([x[1] for x in X.UL]) + sum([x[2] for x in X.UL]) >= minutil:
+            exULs = []
+            for Y in ULs[ULs.index(X)+1:]:
+                # UtilityList(X + Y[-1], Construct(P.UL, X.UL, Y.UL))
+                # print(P.item, X.item, Y.item)
+                exULs.append(UtilityList(X.item + Y.item[-1], Construct(P.UL, X.UL, Y.UL)))
+            HUI_Miner(X, exULs, minutil)
+
+
+HUI_Miner(UtilityList('', None), IUL_Obj, 20)
+
+
